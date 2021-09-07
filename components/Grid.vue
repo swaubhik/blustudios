ba0b6e103a396209f6b532196f31af930ace70e5
raycazy <81694463+raycazy@users.noreply.github.com>
2021-09-07 22:38:15 +0530

    news feed shadow remove and resize

--- components/Grid.vue
+++ components/Grid.vue
@@ -2,11 +2,11 @@
   <div class="flex justify-center items-center mt-20 p-8">
     <div class="gallery overflow-hidden lg:overflow-visible">
       <img
-        class="images p-[10px] md:p-4 m-1 md:m-2 lg:p-8"
+        class="images p-[10px] m-auto  md:p-4 m-auto lg:p-8 py-10 sm:p-2 m-auto"
         v-for="image in images"
         :key="image.id"
         :src="image.pathLong"
-        style="width:85%;"
+        style="width:70%;"     
       />
     </div>
   </div>
@@ -69,12 +69,20 @@ export default {
   transform: translateY(100px);
   position: relative;
 }
+.images:nth-child(1) {
+  transform: translateY(-70px);
+  position: relative;
+}
 
 @media (max-width: 991px) {
   .gallery {
     -webkit-column-count: 2;
     -moz-column-count: 2;
     column-count: 2;
+  }
+  .images {
+    width: 400px;
+     padding: 2.5em 0.5em;
   }
   .images:nth-child(5n) {
   transform: scale(1.1);
@@ -100,5 +108,25 @@ export default {
     -moz-column-width: 100%;
     column-width: 100%;
   }
+  img{
+    width: 90%;
+  }
+   .images {
+     padding: 40px 0px;
+     margin: auto;
+  }
+  .images:nth-child(5n) {
+  transform: scale(1.1);
+  margin: 10px;
+}
+.images:nth-child(2n) {
+  transform: translateX(40px);
+  position: relative;
+}
+
+.images:nth-child(1) {
+  transform: translateY(-70px);
+  position: relative;
+}
 }
 </style>
