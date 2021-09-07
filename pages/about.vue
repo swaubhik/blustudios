14ec13fdd7a1ca3f6683807fe0d13fd89a256d35
swaubhik <b18csel299@cit.ac.in>
2021-09-07 20:32:24 +0530

    :sparkles: about us

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="relative">
     <div
       class="relative w-auto h-[85vh] bg-about-hero bg-center bg-no-repeat bg-fixed bg-cover bg-black"
     >
@@ -69,5 +69,130 @@
         />
       </div>
     </div>
+    <div class="container p-20 max-w-7xl text-center mx-auto">
+      <h4 class="text-5xl uppercase">HOW WE WORK?</h4>
+      <p class="mt-10">
+        As believers of balance between personal & professional, we curate the
+        intricacies and aesthetics that let you tell YOUR brand’s story, the
+        best way possible. This, of course, involves a process of rigorous
+        research by the team to understand the nitty-gritty and your brand’s
+        core values. Following this, we put our minds to weaving a narrative
+        that adheres to them. The next, and most crucial step becomes
+        transforming that story into stunning & compelling visuals. Planning
+        regarding the logistics, design, script, models, and the rest of the
+        production starts as we break down our team into departments. <br />
+        As a contemporary company with experience, we execute ‘best of both
+        worlds’ strategies that bring you original, more importantly, excellent
+        quality work.
+      </p>
+      <p>
+        <span class="block mt-40 text-lg uppercase font-bold"
+          >STAY CONNECTED</span
+        >
+        Want to be the first one to know about our work? Follow us on:
+      </p>
+      <div class="my-8 flex space-x-4 justify-center items-center">
+        <a href="#">
+          <svg
+            width="51"
+            height="50"
+            viewBox="0 0 51 50"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M36.75 2.4873H14.25C8.0368 2.4873 3 7.5241 3 13.7373V36.2373C3 42.4505 8.0368 47.4873 14.25 47.4873H36.75C42.9632 47.4873 48 42.4505 48 36.2373V13.7373C48 7.5241 42.9632 2.4873 36.75 2.4873Z"
+              stroke="black"
+              stroke-width="4.4972"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M34.4998 23.5705C34.7775 25.443 34.4577 27.3555 33.5858 29.0358C32.7139 30.7161 31.3344 32.0787 29.6435 32.9298C27.9526 33.7808 26.0363 34.0771 24.1673 33.7763C22.2984 33.4756 20.5718 32.5932 19.2332 31.2546C17.8946 29.916 17.0122 28.1895 16.7115 26.3205C16.4107 24.4515 16.707 22.5352 17.5581 20.8443C18.4092 19.1534 19.7717 17.7739 21.452 16.902C23.1323 16.0302 25.0448 15.7103 26.9173 15.988C28.8274 16.2712 30.5957 17.1613 31.9611 18.5267C33.3265 19.8921 34.2166 21.6604 34.4998 23.5705Z"
+              stroke="black"
+              stroke-width="4.4972"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M37.8738 12.6123H37.8973"
+              stroke="black"
+              stroke-width="4.4972"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </a>
+        <a href="#">
+          <svg
+            width="51"
+            height="51"
+            viewBox="0 0 51 51"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M8.10917 2.05778L42.4832 2.31198C45.6932 2.33572 48.2762 4.95723 48.2532 8.1673L47.9982 42.5414C47.9752 45.7515 45.3532 48.3345 42.1432 48.3108L7.76916 48.0566C4.55916 48.0328 1.97617 45.4113 2.00017 42.2012L2.25416 7.82714C2.27816 4.61707 4.89917 2.03404 8.10917 2.05778Z"
+              stroke="black"
+              stroke-width="4"
+            />
+            <path
+              d="M18.1591 37.6328L12.8461 37.5934L12.9701 20.8751L18.2821 20.9145L18.1591 37.6328ZM15.6441 18.5511C13.9251 18.5384 12.6841 17.2793 12.6971 15.5606C12.7101 13.8419 14.1251 12.6023 15.6881 12.6139C17.4061 12.6266 18.6471 13.8857 18.6341 15.6044C18.6221 17.3232 17.3621 18.5638 15.6441 18.5511ZM37.5331 37.7761L32.2211 37.7367L32.2881 28.6744C32.3081 26.0183 31.2201 25.229 29.6571 25.2175C28.0951 25.2059 26.5231 26.4444 26.5061 28.788L26.4401 37.694L21.1271 37.6548L21.2511 20.9365L26.2511 20.9734L26.2341 23.317C26.7101 22.2269 28.5981 20.5219 31.2541 20.5416C34.2231 20.5635 37.3351 22.3054 37.2971 27.4616L37.2211 37.7738L37.5331 37.7761Z"
+              fill="black"
+            />
+          </svg>
+        </a>
+      </div>
+      <p>
+        <span class="block mt-40 text-lg uppercase font-bold"
+          >ASPIRE & INSPIRE</span
+        >
+
+        We’re always looking for new sources of inspiration, feel free to reach
+        out to us for sharing ideas, or even conversation about art over a cup
+        of coffee. At the end of the day, it’s always delightful to have a
+        community.
+        <br />
+        Contact Us/ connect with our team
+      </p>
+    </div>
+    <div class="absolute bottom-10 right-10">
+      <button @click="scrollToTop">
+        <svg
+          width="40"
+          height="40"
+          viewBox="0 0 63 63"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="31.5" cy="31.5" r="31.5" fill="#F5F5F5" />
+          <path
+            d="M15 37L29.3473 22.218C30.5256 21.0039 32.4744 21.0039 33.6527 22.218L48 37"
+            stroke="#414141"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    scrollToTop() {
+      let currentScroll = document.documentElement.scrollTop,
+        int = setInterval(frame, 6);
+
+      function frame() {
+        if (0 > currentScroll) clearInterval(int);
+        else {
+          currentScroll = currentScroll - 12;
+          document.documentElement.scrollTop = currentScroll;
+        }
+      }
+    }
+  }
+};
+</script>
